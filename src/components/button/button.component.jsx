90e78d739b19c987b3b16e123628cc3727a01695
Prince-973 <prinsvaghasiya.it21@scet.ac.in>
2025-01-25 15:28:13 +0530

    fix bug in cardElement

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -4,6 +4,7 @@ import {
   GoogleSignInButton,
   InvertedButton,
 } from "./button.style.jsx";
+import { SpinnerContainer } from "../spinner/spinner.style.jsx";
 export const BUTTON_TYPE_CLASSES = {
   base: "base",
   google: "google-sign-in",
@@ -17,9 +18,13 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
   }[buttonType] || BaseButton);
 
-function Button({ children, buttonType, ...otherProps }) {
+function Button({ children, buttonType, isLoading, ...otherProps }) {
   const CustomButton = getButton(buttonType);
-  return <CustomButton {...otherProps}>{children}</CustomButton>;
+  return (
+    <CustomButton {...otherProps}>
+      {isLoading ? <SpinnerContainer /> : children}
+    </CustomButton>
+  );
 }
 
 export default Button;
