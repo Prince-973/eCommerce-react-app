be78b8830876b563835db4a41ba1ad8b1716fb4f
Prince-973 <prinsvaghasiya.it21@scet.ac.in>
2025-01-27 14:25:43 +0530

    add the memo and lazy/suspense for optimization of code

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
-
+import { gql, useMutation, useQuery } from "@apollo/client";
 import ProductCard from "../../components/product-card/product-card.component";
 
 import { CategoriesContext } from "../../contexts/categories.context";
@@ -8,14 +8,62 @@ import { CategoriesContext } from "../../contexts/categories.context";
 import { CategoryContainer, Title } from "./category.styles";
 import Spinner from "../../components/spinner/spinner.component";
 
+const GET_CATEGORY = gql`
+  query ($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      title
+      id
+      items {
+        name
+        id
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
+const SET_CATEGORY = gql`
+  mutation ($category: Category!) {
+    addCategory(category: $category) {
+      title
+      id
+      items {
+        name
+        id
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  // const { categoriesMap, loading } = useContext(CategoriesContext);
+  // const [addCategory, { loading, error, data }] = useMutation(SET_CATEGORY);
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category,
+    },
+  });
 
+  // addCategory({ variables: { category: category } });
+  console.log(data);
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if (data) {
+      const {
+        getCollectionsByTitle: { items },
+      } = data;
+      setProducts(items);
+    }
+  }, [category, data]);
+
+  const [products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   setProducts(categoriesMap[category]);
+  // }, [category, categoriesMap]);
 
   return (
     <Fragment>
